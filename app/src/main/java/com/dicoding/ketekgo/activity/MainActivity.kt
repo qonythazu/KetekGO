5c1e80c2f0878fbb2e1470cc5089895861132816
qonythazu <qonitaarif5@gmail.com>
2023-11-20 21:13:04 +0800

    first commit

---
+++ app/src/main/java/com/dicoding/ketekgo/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.ketekgo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.ketekgo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
