749d2bff7d9a9de04841f1e984303f44b653f1ec
qonythazu <qonitaarif5@gmail.com>
2023-11-30 01:39:07 +0800

    add booking and destination detail page

--- app/src/main/java/com/dicoding/ketekgo/fragment/BookingDetailFragment.kt
+++ app/src/main/java/com/dicoding/ketekgo/fragment/BookingDetailFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProfileDriverFragment.newInstance] factory method to
+ * Use the [BookingDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfileDriverFragment : Fragment() {
+class BookingDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class ProfileDriverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_driver, container, false)
+        return inflater.inflate(R.layout.fragment_booking_detail, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class ProfileDriverFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileDriverFragment.
+         * @return A new instance of fragment BookingDetailFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProfileDriverFragment().apply {
+            BookingDetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/com/dicoding/ketekgo/fragment/driver/ProfileDriverFragment.kt
@@ -0,0 +1,18 @@
+package com.dicoding.ketekgo.fragment.driver
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dicoding.ketekgo.R
+
+class ProfileDriverFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_driver, container, false)
+    }
+}
