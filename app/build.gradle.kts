36196de7dc0b4c3483e3de1ee209b3061492ccef
qonythazu <qonitaarif5@gmail.com>
2023-12-19 00:52:59 +0800

    add login logout using firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,6 +57,10 @@ dependencies {
     // coroutine support
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    //firebase
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/dicoding/ketekgo/activity/MainActivity.kt
+++ app/src/main/java/com/dicoding/ketekgo/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dicoding.ketekgo.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,19 +9,38 @@ import androidx.navigation.ui.setupWithNavController
 import com.dicoding.ketekgo.R
 import com.dicoding.ketekgo.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+
+        if (firebaseUser == null) {
+            // Not signed in, launch the Auth activity
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+            return
+        }
 
         val navView: BottomNavigationView = binding.bottomNav
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_home_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         navView.setupWithNavController(navController)
+
+        binding.logoutText.setOnClickListener {
+            signOut()
+        }
     }
     fun hideBottomNavigationBar() {
         binding.bottomNav.visibility = View.GONE
@@ -28,5 +48,11 @@ class MainActivity : AppCompatActivity() {
 
     fun showBottomNavigationBar() {
         binding.bottomNav.visibility = View.VISIBLE
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        startActivity(Intent(this, AuthActivity::class.java))
+        finish()
     }
 }
